8cf2571a1b410917dc0fa7293a4918c13d7a92aa
Iran Adryan <contatoiranadryan@gmail.com>
2024-01-17 18:06:58 -0300

    treat api url when is on cloud envitonment

--- src/services/utils/api.ts
+++ src/services/utils/api.ts
@@ -1,3 +1,7 @@
 import HttpClient from './HttpClient';
 
-export const api = new HttpClient('http://localhost:3001');
+export const api = new HttpClient(
+  import.meta.env.VITE_ENVIRONMENT === 'cloud'
+    ? 'http://10.100.52.87:3001'
+    : 'http://localhost:3001',
+);
