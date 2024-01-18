d225eff82a44e39bc6293a3d2b197c0a2bc4d72d
Iran Adryan <contatoiranadryan@gmail.com>
2024-01-18 09:03:17 -0300

    fix api base url when is on cloud

--- src/services/utils/api.ts
+++ src/services/utils/api.ts
@@ -2,6 +2,6 @@ import HttpClient from './HttpClient';
 
 export const api = new HttpClient(
   import.meta.env.VITE_ENVIRONMENT === 'cloud'
-    ? 'http://10.100.52.87:3001'
+    ? 'http://10.101.20.220:3001'
     : 'http://localhost:3001',
 );
