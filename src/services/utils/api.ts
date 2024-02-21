035c90130bd5a6afbad13b8087921c3198b734cb
Iran Adryan <contatoiranadryan@gmail.com>
2024-02-21 10:18:58 -0300

    feat: update api cloud url

--- src/services/utils/api.ts
+++ src/services/utils/api.ts
@@ -2,6 +2,6 @@ import HttpClient from './HttpClient';
 
 export const api = new HttpClient(
   import.meta.env.VITE_ENVIRONMENT === 'cloud'
-    ? 'http://10.101.20.220:3001'
+    ? 'http://api.dashboard.gsafra.com'
     : 'http://localhost:3001',
 );
