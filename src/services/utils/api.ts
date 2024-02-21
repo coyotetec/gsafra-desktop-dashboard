508228e36a8552e00c0d47d362441c9390430371
Iran Adryan <contatoiranadryan@gmail.com>
2024-02-21 10:23:54 -0300

    fix: change url protocol to https on cloud

--- src/services/utils/api.ts
+++ src/services/utils/api.ts
@@ -2,6 +2,6 @@ import HttpClient from './HttpClient';
 
 export const api = new HttpClient(
   import.meta.env.VITE_ENVIRONMENT === 'cloud'
-    ? 'http://api.dashboard.gsafra.com'
+    ? 'https://api.dashboard.gsafra.com'
     : 'http://localhost:3001',
 );
