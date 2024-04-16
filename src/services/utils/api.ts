005163c17524a581cbc720e227e0d9c541ac599b
Iran Adryan <contatoiranadryan@gmail.com>
2024-04-16 09:22:19 -0300

    change db config to agrogil specific database

--- src/services/utils/api.ts
+++ src/services/utils/api.ts
@@ -2,6 +2,6 @@ import HttpClient from './HttpClient';
 
 export const api = new HttpClient(
   import.meta.env.VITE_ENVIRONMENT === 'cloud'
-    ? 'https://api.dashboard.gsafra.com'
+    ? 'http://node179752-dbgsafra.nordeste-idc.saveincloud.net/'
     : 'http://localhost:3001',
 );
