e3eab8a415d2fe84e58fe88abd760f6368c81235
Iran Adryan <contatoiranadryan@gmail.com>
2024-04-16 09:51:44 -0300

    updating dist

--- src/services/utils/api.ts
+++ src/services/utils/api.ts
@@ -2,6 +2,6 @@ import HttpClient from './HttpClient';
 
 export const api = new HttpClient(
   import.meta.env.VITE_ENVIRONMENT === 'cloud'
-    ? 'http://node179752-dbgsafra.nordeste-idc.saveincloud.net/'
+    ? 'http://node179752-dbgsafra.nordeste-idc.saveincloud.net'
     : 'http://localhost:3001',
 );
