6fb6c400fb616fe3fb6eb52351a26a03650527dd
raul.souza <raulcesar54@hotmail.com>
2025-03-30 16:47:03 -0400

    feat: change api endpoint

--- src/services/utils/api.ts
+++ src/services/utils/api.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+// export const api = axios.create({
+//   baseURL:
+//     import.meta.env.VITE_ENVIRONMENT === 'cloud'
+//       ? 'https://api.dashboard.gsafra.com'
+//       : 'http://localhost:3001',
+// });
 export const api = axios.create({
   baseURL:
     import.meta.env.VITE_ENVIRONMENT === 'cloud'
-      ? 'https://api.dashboard.gsafra.com'
+      ? 'https://92.246.130.170'
       : 'http://localhost:3001',
 });
